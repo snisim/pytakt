dd5b6060b7f5ca82c05db9f61b3d82eeea87eb80
snisim <snisim>
2026-08-04 18:12:23 +0900

    - Names defined in "sc" submodules (such as "ctrl") can now be   accessed as "pytakt.ctrl" instead of "pytakt.sc.ctrl" when using   "import pytakt". When using "from pytakt import *", these   names must still be accessed through "sc" (e.g. "sc.ctrl").

--- pytakt/__init__.py
+++ pytakt/__init__.py
@@ -1,3 +1,4 @@
+import pytakt.context as _context  # to avoid confusion with the context() func
 from pytakt.context import *
 from pytakt.pitch import *
 from pytakt.event import *
@@ -5,8 +6,9 @@ from pytakt.constants import *
 from pytakt.score import *
 from pytakt.effector import *
 import pytakt.sc as sc
-from pytakt.sc import note, rest
+from pytakt.sc import *
 from pytakt.smf import *
+import pytakt.mml as _mml  # to avoid confusion with the mml() func
 from pytakt.mml import *
 from pytakt.scale import *
 from pytakt.chord import *
@@ -16,3 +18,11 @@ from pytakt.utils import *
 from pytakt.text import *
 import pytakt.gm as gm
 from pytakt._version import __version__
+
+
+__all__ = _context.__all__ + pitch.__all__ + event.__all__ + \
+    [k for k in dir(constants) if not k.startswith('_')] + \
+    score.__all__ + effector.__all__ + smf.__all__ + _mml.__all__ + \
+    scale.__all__ + chord.__all__ + timemap.__all__ + \
+    interpolator.__all__ + utils.__all__  + text.__all__ + \
+    ['note', 'rest', 'sc', 'gm']
